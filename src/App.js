5fa5f5719b38cdc46e25e037d5341147a519cfc4
JakubP23 <pecakj1@montclair.edu>
2022-03-21 13:58:10 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import LandingPage from '/Users/kuba2001/Desktop/savnac/my-app/src/pages/LandingPage.js'
+import LoginPage from '/Users/kuba2001/Desktop/savnac/my-app/src/pages/LoginPage.js'
+import RegisterPage from '/Users/kuba2001/Desktop/savnac/my-app/src/pages/RegisterPage.js'
+import ForgetPasswordPage from '/Users/kuba2001/Desktop/savnac/my-app/src/pages/ForgetPasswordPage.js'
+import HomePage from '/Users/kuba2001/Desktop/savnac/my-app/src/pages/HomePage.js'
+
+import '/Users/kuba2001/Desktop/savnac/my-app/src/App.css'
+
+const App = () => {
+        return (
+        <Router>
+                <Routes>
+                    <Route exact path="/" element={ <LandingPage/> } />
+                    <Route path="/login" element={ <LoginPage/> } />
+                    <Route path="/register" element={ <RegisterPage/> } />
+                    <Route path="/forget-password" element={ <ForgetPasswordPage/> } />
+                    <Route path="/home" element={ <HomePage/> } />
+                </Routes>
+                <Footer />
+        </Router>
+    )
+}
+
+const Footer = () => {
+    return (
+        <p className="text-center" style={ FooterStyle }>Created by <a href="#" target="_blank" rel="noopener noreferrer">Savnac Team</a></p>
+    )
+}
+
+const FooterStyle = {
+    background: "#222",
+    fontSize: ".8rem",
+    color: "#fff",
+    position: "absolute",
+    bottom: 0,
+    padding: "1rem",
+    margin: 0,
+    width: "100%",
+    opacity: ".5"
 }
 
 export default App;
